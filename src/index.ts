aa86737e8bdd428a49882f0705b28ae3dfbcb606
Kazumi Inada <hello@nandenjin.com>
2024-04-02 23:20:53 +0900

    feat: Accept type `ContentDir` for `options.contentDirs`

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
-import { Options } from "./types";
+import { ContentDir, Options } from "./types";
 
 import fs from "fs";
 import path from "path";
 import frontMatter from "front-matter";
 
-function getSidebarItems(dir: string[], currentRoot: string | undefined, root: string | undefined, options: Options): object[] {
+function getSidebarItems(dir: string[] | ContentDir[], currentRoot: string | undefined, root: string | undefined, options: Options): object[] {
 	return dir
-    .filter(e => e.endsWith('.md') || fs.statSync(path.resolve(currentRoot ?? '/', e)).isDirectory())
-    .map((e: string) => {
+    .filter(e => {
+      const itemPath = typeof e === 'string' ? e : e.path
+      return (itemPath.endsWith('.md') || fs.statSync(path.resolve(currentRoot ?? '/', itemPath)).isDirectory())
+    })
+    .map((entry: string | ContentDir) => {
+      const e = typeof entry === 'string' ? entry : entry.path;
       const childDir = path.resolve(currentRoot ?? '/', e);
       if (fs.statSync(childDir).isDirectory()) {
         const items = getSidebarItems(fs.readdirSync(childDir), childDir, root, options)
         const fileName = e.split('/').pop() ?? ''
+        
+        let title: string;
+        if (typeof entry !== 'string' && entry.title) {
+          title = entry.title
+        } else {
+          title = ((fileName.charAt(0).toUpperCase() + fileName.slice(1))).replaceAll('-', ' ')
+        }
+
         return items.length ?  {
-          text: ((fileName.charAt(0).toUpperCase() + fileName.slice(1))).replaceAll('-', ' '),
+          text: title,
           collapsible: options.collapsible,
           collapsed: options.collapsed,
           items
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,12 @@
 export interface Options {
   contentRoot?: string;
-  contentDirs?: string[] | null;
+  contentDirs?: string[] | ContentDir[] | null;
   collapsible?: boolean;
   collapsed?: boolean;
   useFrontMatter?: boolean;
+}
+
+export interface ContentDir {
+  path: string,
+  title?: string
 }
